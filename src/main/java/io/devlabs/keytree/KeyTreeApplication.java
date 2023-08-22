42d8c830bc092ad802e6a1f59d607afef7dab782
Yonghwi Kim <yh0921k@gmail.com>
2023-08-22 22:50:24 +0900

    init: KeyTree 프로젝트 초기화
    build.gradle
    - Spring Web, Spring Data JPA, Lombok, MySQL Driver 의존성 추가
    
    .gitignore
    - IntelliJ, MacOS, Java, Gradle, Git에 대한 불필요 추적 제외 패턴 추가

---
+++ src/main/java/io/devlabs/keytree/KeyTreeApplication.java
@@ -0,0 +1,11 @@
+package io.devlabs.keytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyTreeApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KeyTreeApplication.class, args);
+  }
+}
