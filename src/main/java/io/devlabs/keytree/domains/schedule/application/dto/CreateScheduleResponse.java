968faa60813f525f9ed66a06ddaa740299a516ab
Yonghwi Kim <yh0921k@gmail.com>
2023-08-24 17:00:00 +0900

    feat: Schedule 등록 DTO LocalDateTime 관련 JsonFormat 설정 추가
    CreateScheduleRequest.java
    CreateScheduleResponse.java
    - LocalDateTime 필드에 대해 주어진 패턴 적용

--- src/main/java/io/devlabs/keytree/domains/schedule/application/dto/CreateScheduleResponse.java
+++ src/main/java/io/devlabs/keytree/domains/schedule/application/dto/CreateScheduleResponse.java
@@ -2,6 +2,7 @@ package io.devlabs.keytree.domains.schedule.application.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 @Getter
@@ -10,8 +11,13 @@ import lombok.*;
 @Builder
 public class CreateScheduleResponse {
   private Long id;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime startedAt;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime finishedAt;
+
   private String title;
   private String contents;
 }
