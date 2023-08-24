50a11cbd2fb5071f5f5ae4f20b36ac7e172e03ee
Yonghwi Kim <yh0921k@gmail.com>
2023-08-24 17:00:00 +0900

    feat: ScheduleRepository 구현
    ScheduleRepository.java
    - Schedule 관련 JpaRepository 구현

---
+++ src/main/java/io/devlabs/keytree/domains/schedule/domain/ScheduleRepository.java
@@ -0,0 +1,7 @@
+package io.devlabs.keytree.domains.schedule.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {}
