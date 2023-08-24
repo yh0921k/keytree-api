07719f2ac3fa7b8374d26015f8de18fffa1fd9bc
Yonghwi Kim <yh0921k@gmail.com>
2023-08-24 17:00:00 +0900

    feat: Schedule 엔티티 빌더패턴 적용
    Schedule.java
    - 빌더패턴 사용을 위한 어노테이션 추가

--- src/main/java/io/devlabs/keytree/domains/schedule/domain/Schedule.java
+++ src/main/java/io/devlabs/keytree/domains/schedule/domain/Schedule.java
@@ -3,13 +3,14 @@ package io.devlabs.keytree.domains.schedule.domain;
 import io.devlabs.keytree.commons.jpa.BaseTimeEntity;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
+@Builder
 public class Schedule extends BaseTimeEntity {
 
   @Id
