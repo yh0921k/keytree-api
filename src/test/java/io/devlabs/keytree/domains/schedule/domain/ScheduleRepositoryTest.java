71670f594aa3a76546f0f647caf281b47ec98ccc
Yonghwi Kim <yh0921k@gmail.com>
2023-08-25 17:00:00 +0900

    test: Schedule 리스트 조회 로직 테스트
    ScheduleRepositoryTest.java
    - JpaRepository를 대상으로 findAll() 테스트 및 결과 확인
    - 내부 Schedule 생성 메서드 구현

--- src/test/java/io/devlabs/keytree/domains/schedule/domain/ScheduleRepositoryTest.java
+++ src/test/java/io/devlabs/keytree/domains/schedule/domain/ScheduleRepositoryTest.java
@@ -1,22 +1,21 @@
 package io.devlabs.keytree.domains.schedule.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.*;
-
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class ScheduleRepositoryTest {
 
-  @Autowired
-  ScheduleRepository scheduleRepository;
+  @Autowired ScheduleRepository scheduleRepository;
 
   @Test
   @DisplayName("일정 엔티티 저장")
@@ -29,13 +28,14 @@ class ScheduleRepositoryTest {
         LocalDateTime.parse(
             "2023-08-27 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
-    Schedule schedule = Schedule.builder()
-        .startedAt(startedAt)
-        .finishedAt(finishedAt)
-        .title("Title")
-        .contents("Contents")
-        .userId(1L)
-        .build();
+    Schedule schedule =
+        Schedule.builder()
+            .startedAt(startedAt)
+            .finishedAt(finishedAt)
+            .title("Title")
+            .contents("Contents")
+            .userId(1L)
+            .build();
 
     // when
     Schedule savedSchedule = scheduleRepository.save(schedule);
@@ -47,5 +47,40 @@ class ScheduleRepositoryTest {
     assertThat(savedSchedule.getStartedAt()).isEqualTo(schedule.getStartedAt());
     assertThat(savedSchedule.getFinishedAt()).isEqualTo(schedule.getFinishedAt());
     assertThat(savedSchedule.getUserId()).isEqualTo(schedule.getUserId());
+  }
+
+  @Test
+  @DisplayName("일정 엔티티 리스트 조회")
+  void findAll() {
+    // given
+    Schedule firstSchedule = createSchedule();
+    Schedule secondSchedule = createSchedule();
+    scheduleRepository.saveAll(List.of(firstSchedule, secondSchedule));
+
+    // when
+    List<Schedule> schedules = scheduleRepository.findAll();
+    List<Long> scheduleIds = schedules.stream().map(Schedule::getId).toList();
+
+    // then
+    assertThat(schedules.size()).isEqualTo(2);
+    assertThat(scheduleIds.contains(1L)).isTrue();
+    assertThat(scheduleIds.contains(2L)).isTrue();
+  }
+
+  private Schedule createSchedule() {
+    LocalDateTime startedAt =
+        LocalDateTime.parse(
+            "2023-08-27 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    LocalDateTime finishedAt =
+        LocalDateTime.parse(
+            "2023-08-27 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+    return Schedule.builder()
+        .startedAt(startedAt)
+        .finishedAt(finishedAt)
+        .title("Title")
+        .contents("Contents")
+        .userId(1L)
+        .build();
   }
 }
