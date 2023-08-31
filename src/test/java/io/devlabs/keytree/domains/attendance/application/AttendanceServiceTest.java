8300e9c29a3ed0c3f50e3343316da40432f91191
ujamdev <ujamdev@gmail.com>
2023-08-31 21:23:42 +0900

    feat: 출석 목록 조회 로직 구현
    AttendanceService.java
    AttendanceServiceTest.java
     - 출석 목록 반환 로직 구성
     - 테스트 코드는 2개의 출석 엔티티를 생성 후 출석 목록 반환과 사이즈가 맞는지 검증
     - id를 포함하고 있는지 검증

--- src/test/java/io/devlabs/keytree/domains/attendance/application/AttendanceServiceTest.java
+++ src/test/java/io/devlabs/keytree/domains/attendance/application/AttendanceServiceTest.java
@@ -5,6 +5,8 @@ import io.devlabs.keytree.domains.attendance.application.dto.CreateStartAttendan
 import io.devlabs.keytree.domains.attendance.application.dto.CreateStartAttendanceResponse;
 import io.devlabs.keytree.domains.attendance.domain.Attendance;
 import io.devlabs.keytree.domains.attendance.domain.AttendanceRepository;
+import io.devlabs.keytree.domains.schedule.application.dto.CreateScheduleResponse;
+import io.devlabs.keytree.domains.schedule.domain.Schedule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -46,6 +49,27 @@ public class AttendanceServiceTest {
         assertThat(response.getStartedAt()).isEqualTo(attendance.getStartedAt());
     }
 
+    @Test
+    @DisplayName("출근 목록 조회")
+    void getAttendance() {
+        // given
+        Attendance firstAttendance = createStartAttendanceEntity(1L, createStartAttendanceRequest());
+        Attendance secondAttendance = createStartAttendanceEntity(2L, createStartAttendanceRequest());
+        List<Attendance> attendances = List.of(firstAttendance, secondAttendance);
+        CreateStartAttendanceRequest request = createStartAttendanceRequest();
+        Attendance attendance = createStartAttendanceEntity(1L, createStartAttendanceRequest());
+
+        when(attendanceRepository.findAll()).thenReturn(attendances);
+
+        // when
+        List<Attendance> foundAttendances = attendanceService.getAttendances();
+        List<Long> attendanceIds = foundAttendances.stream().map(Attendance::getId).toList();
+
+        // then
+        assertThat(foundAttendances.size()).isEqualTo(attendances.size());
+        assertThat(attendanceIds.contains(firstAttendance.getId())).isTrue();
+        assertThat(attendanceIds.contains(secondAttendance.getId())).isTrue();
+    }
 
     private Attendance createStartAttendanceEntity(Long attendanceId, CreateStartAttendanceRequest request) {
         return Attendance.builder()
