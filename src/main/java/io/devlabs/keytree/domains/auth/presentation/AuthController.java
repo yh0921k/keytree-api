6b0862db92ced2096d9901fa1cdfce9872dec92b
Yonghwi Kim <yh0921k@gmail.com>
2023-09-09 17:00:00 +0900

    feat: 사용자 로그인 API 구현
    application.yml
    - ddl-auto를 update로 설정시 테스트 코드 케이스별 롤백이 되지 않아 문제 발생
    - 추후 어플리케이션 구동시점에 필요한 데이터가 존재하낟면 스크립트로 처리 예정
    
    AuthController.java
    - 존재하는 세션 무효화
    - 새로운 세션을 생성하여 사용자 이메일 정보를 포함
    - 세션 만료시간은 1시간 설정
    
    AuthControllerTest.java
    - 초기 사용자 데이터를 한 개 저장한 후 테스트 진행
    - 요청 파라미터를 구성하여 로그인 API 호출시 JSESSIONID가 쿠키에 포함됨

---
+++ src/main/java/io/devlabs/keytree/domains/auth/presentation/AuthController.java
@@ -0,0 +1,32 @@
+package io.devlabs.keytree.domains.auth.presentation;
+
+import io.devlabs.keytree.domains.auth.application.AuthService;
+import io.devlabs.keytree.domains.auth.application.dto.SignInUserRequest;
+import io.devlabs.keytree.domains.user.application.dto.CreateUserResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/signIn")
+  public ResponseEntity<CreateUserResponse> signIn(
+      HttpServletRequest request, @RequestBody SignInUserRequest signInUserRequest) {
+    request.getSession().invalidate();
+
+    HttpSession createdSession = request.getSession(true);
+    createdSession.setAttribute("email", signInUserRequest.getEmail());
+    createdSession.setMaxInactiveInterval(3600);
+
+    CreateUserResponse responseData = authService.signIn(signInUserRequest, createdSession.getId());
+    return ResponseEntity.ok(responseData);
+  }
+}
