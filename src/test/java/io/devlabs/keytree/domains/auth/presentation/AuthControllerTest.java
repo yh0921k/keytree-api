cd8a81e4984899b09af4326321148bc890fd0154
ujamdev <ujamdev@gmail.com>
2023-09-10 15:11:29 +0900

    feat: 로그아웃 API 핸들러 구현
    AuthController.java
    AuthControllerTest.java
     - 로그아웃 핸들러 구현
     - 로그아웃 API 호출 시 정상 작동하는지 검증

--- src/test/java/io/devlabs/keytree/domains/auth/presentation/AuthControllerTest.java
+++ src/test/java/io/devlabs/keytree/domains/auth/presentation/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package io.devlabs.keytree.domains.auth.presentation;
 
+<<<<<<< HEAD
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 
@@ -17,10 +18,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+=======
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+>>>>>>> 99b7042 (feat: 로그아웃 API 핸들러 구현)
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+<<<<<<< HEAD
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -78,4 +88,43 @@ class AuthControllerTest {
     assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     assertThat(response.cookie("JSESSIONID")).isNotNull();
   }
+=======
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("로그아웃 API")
+    @Test
+    void logout() {
+        // given
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given()
+                        .log()
+                        .all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when()
+                        .post("/logout")
+                        .then()
+                        .log()
+                        .all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+>>>>>>> 99b7042 (feat: 로그아웃 API 핸들러 구현)
 }
