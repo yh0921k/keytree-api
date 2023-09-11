aedaf1f1c1d02ca7a77c1a4696aa783850609aab
ujamdev <ujamdev@gmail.com>
2023-09-11 23:50:25 +0900

    feat: 회사 계정 엔티티 및 repository 구현
    CompanyAccount.java
     - 회사 계정에 대한 테이블
     - 아이디, 이름, 로그인아이디, 로그인비밀번호, 권한 필드로 구성
     - 회사마다 아이디가 다수로 생길 수 있어 정규화하여 엔티티 구현
    CompanyAccountRepository.java
     - 회사 테이블에 대한 Repository 구현

---
+++ src/main/java/io/devlabs/keytree/domains/company/domain/CompanyAccount.java
@@ -0,0 +1,40 @@
+package io.devlabs.keytree.domains.company.domain;
+
+import io.devlabs.keytree.commons.jpa.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class CompanyAccount extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("회사 계정 고유 아이디")
+    private Long id;
+
+    @Column
+    @NotNull
+    @Comment("계정 이름")
+    private String name;
+
+    @Column
+    @NotNull
+    @Comment("로그인 아이디")
+    private String loginId;
+
+    @Column
+    @NotNull
+    @Comment("로그인 비밀번호")
+    private String password;
+
+    @Column
+    @NotNull
+    @Comment("권한")
+    private Authority authority;
+}
