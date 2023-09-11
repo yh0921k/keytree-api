3e3d80b19612b8032ab64cf972b7cd1f79a7b34b
ujamdev <ujamdev@gmail.com>
2023-09-11 23:38:27 +0900

    feat: 회사 엔티티 및 repository 구현
    Company.java
     - 회사에 대한 테이블
     - 아이디, 이름, 연락처, 주소 필드로 구성
     - 회사 로그인 아이디가 다수 일 경우도 있고 각 아이디마다 부여되는 권한이 다를 수 있기 때문에 회사 로그인 관련 정보는 정규화하여 구성할 예정
    CompanyRepository.java
     - 회사 테이블에 대한 Repository 구현

---
+++ src/main/java/io/devlabs/keytree/domains/company/domain/CompanyRepository.java
@@ -0,0 +1,8 @@
+package io.devlabs.keytree.domains.company.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
