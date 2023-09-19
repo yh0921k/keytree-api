048b445e489168da0e554d096b71c3f6d786edf0
ujamdev <ujamdev@gmail.com>
2023-09-19 21:37:03 +0900

    feat: 충돌 관련 코드들 정리

--- src/main/java/io/devlabs/keytree/domains/auth/presentation/AuthController.java
+++ src/main/java/io/devlabs/keytree/domains/auth/presentation/AuthController.java
@@ -1,7 +1,6 @@
 package io.devlabs.keytree.domains.auth.presentation;
 
 import io.devlabs.keytree.domains.auth.application.AuthService;
-<<<<<<< HEAD
 import io.devlabs.keytree.domains.auth.application.dto.SignInUserRequest;
 import io.devlabs.keytree.domains.user.application.dto.CreateUserResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,21 +29,9 @@ public class AuthController {
     CreateUserResponse responseData = authService.signIn(signInUserRequest, createdSession.getId());
     return ResponseEntity.ok(responseData);
   }
-=======
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequiredArgsConstructor
-public class AuthController {
-
-    private final AuthService authService;
-
-    @PostMapping("/logout")
-    public String logout (HttpServletRequest request) {
-        return authService.logout(request);
-    }
->>>>>>> 99b7042 (feat: 로그아웃 API 핸들러 구현)
+  @PostMapping("/logout")
+  public String logout (HttpServletRequest request) {
+    return authService.logout(request);
+  }
 }
